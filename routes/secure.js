b81653b29b18fa76b0d017946689d53d5f7b468e
Amaan Chowdhury <rahmancm@coventry.ac.uk>
2021-02-06 00:58:36 +0000

    <first commit>

---
+++ routes/secure.js
@@ -0,0 +1,41 @@
+
+import Router from 'koa-router'
+
+const router = new Router({ prefix: '/secure' })
+
+async function checkAuth(ctx, next) {
+	console.log('secure router middleware')
+	console.log(ctx.hbs)
+	if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
+	await next()
+}
+
+router.use(checkAuth)
+
+router.get('/', async ctx => {
+	try {
+		await ctx.render('secure', ctx.hbs)
+	} catch(err) {
+		ctx.hbs.error = err.message
+		await ctx.render('error', ctx.hbs)
+	}
+})
+
+router.get('/search', async ctx => {
+ await ctx.render('search', ctx.hbs)
+})
+
+router.get('/bookforms', async ctx => {
+ await ctx.render('bookforms', ctx.hbs)
+})
+
+router.get('/books', async ctx => {
+ await ctx.render('books', ctx.hbs)
+})
+
+router.get('/checkout', async ctx => {
+ await ctx.render('checkout', ctx.hbs)
+})
+
+
+export default router
