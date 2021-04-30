8efacfa5ef0d04d6c1810831285a9d644aec890e
Amaan Chowdhury <rahmancm@coventry.ac.uk>
2021-04-30 16:52:41 +0000

    tests added

--- unitTests/test-addbook.js
+++ unitTests/test-addbook.js
@@ -1,4 +1,31 @@
 import test from 'ava'
-import Accounts from '../modules/addbook.js'
+import addbook from '../modules/addbook.js'
 
-test ('ADD: error ')
+test('ADD : error if no data entered', async test => {
+	test.plan(1)
+	const bookData = await new addbook()
+	try {
+		await bookData.getAllBooks('')
+		 test.fail('error was not thrown')
+	}catch(err) {
+		test.is(err.message, 'Book was not added')
+	} finally {
+		bookData.close()
+	}
+})
+
+
+test('ADD : error if wrong datatype entered', async test => {
+	test.plan(1)
+	const bookData = await new addbook()
+	try {
+		await bookData.GetAllBooks('$$$$$')
+		test.fail('error was not thrown')
+	}catch(err) {
+		test.is(err.message, 'Book was not added')
+	} finally {
+		bookData.close()
+	}
+})
+
+
